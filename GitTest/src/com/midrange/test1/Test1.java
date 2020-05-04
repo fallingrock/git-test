18838972a05e84020134d54cd4cd478e9c5bb623
david <david@172.16.0.3>
2020-05-04 11:57:33 -0500

    1: issue #1
    Task-Url: https://github.com/fallingrock/git-test/issues/1

--- GitTest/src/com/midrange/test1/Test1.java
+++ GitTest/src/com/midrange/test1/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("whatcha doin'?");
+		System.out.println("another change");
 	}
 
 }
